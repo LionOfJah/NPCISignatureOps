362fbf77d99a8f78867a2e32c6696a2b5c6b134d
Himanshu <himanshu04dagur@gmail.com>
2020-10-08 17:32:16 +0530

    Latest Password Changes

--- src/main/java/com/icicibank/apimgmt/service/impl/SignatureVerificationServiceImpl.java
+++ src/main/java/com/icicibank/apimgmt/service/impl/SignatureVerificationServiceImpl.java
@@ -148,7 +148,7 @@ public class SignatureVerificationServiceImpl implements SignatureVerificationSe
 		{
 			KeyStore ks 			 = KeyStore.getInstance("PKCS12");
 			String keyStoreFile 	 = privateKeyPath;
-			char[] ketStrorePwdArray = "Icici#123".toCharArray();
+			char[] ketStrorePwdArray = "icicneft".toCharArray();
 			keyStoreInputStream 	 = new FileInputStream(keyStoreFile);
 			ks.load(keyStoreInputStream, ketStrorePwdArray);
 
